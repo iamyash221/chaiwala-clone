24a99acba2212b8b0e64454db6e1be16b132632f
Yash Chavda <yashchavda360@gmail.com>
2024-01-06 14:05:31 -0500

    project is done in 3.5 hrs :)

---
+++ src/components/Misc.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import { BsMouse } from "react-icons/bs";
+import "../styles/Misc.scss";
+
+const Misc = () => {
+  return (
+    <>
+      <div className="cursor"></div>
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" data-cursorpointer={true} />
+      </a>
+      <a href="/franchise" className="franchiseBtn" data-cursorpointer={true}>
+        GET A FRANCHISE
+      </a>
+
+      <BsMouse className="scrollBtn" data-cursorpointer={true} />
+    </>
+  );
+};
+
+export default Misc;
